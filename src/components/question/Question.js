08fc885e4b690bca24e44a5c67fa521e1ad6abed
Laurent Cariot <lcariot@sqli.com>
2017-03-13 10:45:00 +0100

    update question component

--- src/components/question/Question.js
+++ src/components/question/Question.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 import { browserHistory } from 'react-router';
 
 var Highlight = require('react-highlight');
-import '../../../node_modules/react-highlight/node_modules/highlight.js/styles/arduino-light.css';
+import '../../../node_modules/react-highlight/node_modules/highlight.js/styles/atom-one-dark.css';
 
-import {Card, CardText, CardMedia, CardHeader} from 'material-ui/Card';
+import {Card, CardText, CardMedia, CardHeader, CardTitle} from 'material-ui/Card';
 import Checkbox from 'material-ui/Checkbox';
 
 import './Question.css';
 
 class Question extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
         this.index = props.params.id - 1;
         this.state = {
@@ -19,11 +19,11 @@ class Question extends Component {
         };
     }
 
-    componentDidMount(){
+    componentDidMount() {
         const question = this.props.question;
-        if(!question){
+        if (!question) {
             browserHistory.push('/themes');
-        }else{
+        } else {
             this.setState({
                 question: question
             });
@@ -32,7 +32,7 @@ class Question extends Component {
 
 
     componentWillReceiveProps(nextProps) {
-        this.index = nextProps.params.id -1;
+        this.index = nextProps.params.id - 1;
         const question = nextProps.question;
         this.setState({
             question: question
@@ -41,7 +41,7 @@ class Question extends Component {
 
 
     handleCheck(index, event, checked) {
-        let question  = this.state.question;
+        let question = this.state.question;
         question.responses[index].checked = checked;
         this.setState({
             question: question
@@ -51,16 +51,23 @@ class Question extends Component {
 
     render() {
         return this.state.question && (
-                <Card className="Question">
+                <Card style={{boxShadow: 'none'}}>
                     <CardHeader
                         title={this.state.question.theme.name}
-                        subtitle={'Question '+ this.state.question.id + ' - ' + this.state.question.title + ' :'}
+                        subtitle={'Question '+ this.state.question.id}
                         avatar={this.state.question.theme.logo}
                     />
+                    <CardTitle title={this.state.question.title + ' :'} />
                     <CardMedia>
-                        <Highlight className={this.state.question.theme.name}>
-                            {this.state.question.code}
-                        </Highlight>
+                        <div className="question-content">
+                            <div>
+                            </div>
+                            <div className="question-code">
+                                <Highlight className={this.state.question.theme.name}>
+                                    {this.state.question.code}
+                                </Highlight>
+                            </div>
+                        </div>
                     </CardMedia>
                     <CardText>
                         {
@@ -69,12 +76,11 @@ class Question extends Component {
                                     onCheck={this.handleCheck.bind(this, index)}
                                     key={index}
                                     label={response.title}
-                                    checked = {response.checked || false}
+                                    checked={response.checked || false}
                                 />)
                         }
                     </CardText>
                 </Card>
-
             );
     }
 }
